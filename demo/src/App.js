0873ebee604f1dd1bb61164f12992f41b93f91d1
Jean-Baptiste Onofré <jbonofre@apache.org>
2022-10-22 14:40:41 +0200

    Add log table

--- demo/src/App.js
+++ demo/src/App.js
@@ -90,7 +90,7 @@ function Content(props) {
           <Billing vms={props.vms} containers={props.containers} rds={props.rds} kafka={props.kafka} kubernetes={props.kubernetes} rancher={props.rancher} cassandra={props.cassandra} activemq={props.activemq} pulsar={props.pulsar} items={props.items} />
         </Route>
 				<Route path="/monitoring" key="monitoring">
-					<Monitoring vms={props.vms} rds={props.rds}  />
+					<Monitoring vms={props.vms} rds={props.rds} kafka={props.kafka} kubernetes={props.kubernetes} />
 				</Route>
 			</Switch>
       </div>
--- demo/src/Monitoring.js
+++ demo/src/Monitoring.js
@@ -1,36 +1,81 @@
-import { Breadcrumb, Row, Col, Card } from 'antd';
+import { Breadcrumb, Row, Col, Card, Table, Divider } from 'antd';
 import { HomeOutlined, MonitorOutlined } from '@ant-design/icons';
 import { Line } from '@ant-design/plots';
 import { Link } from 'react-router-dom';
 
 function Monitoring(props) {
-  var vmData = [];
+  var logData = [];
+  var cpuVmData = [];
   props.vms.forEach((vm) => {
     var itemData = [
       { "time": "08:00", "value": 0, "category": "vm-" + vm.name },
       { "time": "08:30", "value": 10, "category": "vm-" + vm.name },
-      { "time": "09:00", "value": 20, "category": "vm-" + vm.name }
+      { "time": "09:00", "value": 20, "category": "vm-" + vm.name },
+      { "time": "09:30", "value": 10, "category": "vm-" + vm.name },
+      { "time": "10:00", "value": 50, "category": "vm-" + vm.name },
+      { "time": "10:30", "value": 40, "category": "vm-" + vm.name },
+      { "time": "11:00", "value": 35, "category": "vm-" + vm.name },
+      { "time": "11:30", "value": 30, "category": "vm-" + vm.name },
     ];
-    vmData = vmData.concat(itemData);
+    cpuVmData = cpuVmData.concat(itemData);
+    var vmLog = [
+      { timestamp: '2022-10-25 08:12:32', loc: 'vm-' + vm.name, level: 'INFO', message: 'This is a VM log message' }, 
+      { timestamp: '2022-10-25 08:17:14', loc: 'vm-' + vm.name, level: 'INFO', message: 'Another VM log message' }, 
+      { timestamp: '2022-10-25 10:47:14', loc: 'vm-' + vm.name, level: 'WARN', message: 'Ouch, potential issue' }, 
+    ];
+    logData = logData.concat(vmLog);
   }); 
-  var rdsData = [];
+  var cpuRdsData = [];
   props.rds.forEach((rds) => {
     var itemData = [
       { "time": "08:00", "value": 90, "category": "rds-" + rds.name },
       { "time": "08:30", "value": 30, "category": "rds-" + rds.name },
-      { "time": "09:00", "value": 50, "category": "rds-" + rds.name }
+      { "time": "09:00", "value": 50, "category": "rds-" + rds.name },
+      { "time": "09:30", "value": 40, "category": "rds-" + rds.name },
+      { "time": "10:00", "value": 45, "category": "rds-" + rds.name },
+      { "time": "10:30", "value": 40, "category": "rds-" + rds.name },
+      { "time": "11:00", "value": 30, "category": "rds-" + rds.name },
+      { "time": "11:30", "value": 50, "category": "rds-" + rds.name },
     ];
-    rdsData = rdsData.concat(itemData);
+    cpuRdsData = cpuRdsData.concat(itemData);
+  });
+  var cpuKafkaData = [];
+  props.kafka.forEach((kafka) => {
+    var itemData = [
+      { "time": "08:00", "value": 10, "category": "kafka-" + kafka.name },
+      { "time": "08:30", "value": 5, "category": "kafka-" + kafka.name },
+      { "time": "09:00", "value": 5, "category": "kafka-" + kafka.name },
+      { "time": "09:30", "value": 10, "category": "kafka-" + kafka.name },
+      { "time": "10:00", "value": 15, "category": "kafka-" + kafka.name },
+      { "time": "10:30", "value": 20, "category": "kafka-" + kafka.name },
+      { "time": "11:00", "value": 10, "category": "kafka-" + kafka.name },
+      { "time": "11:30", "value": 10, "category": "kafka-" + kafka.name },
+    ];
+    cpuKafkaData = cpuKafkaData.concat(itemData);
+  });
+  var cpuKubernetesData = [];
+  props.kubernetes.forEach((kubernetes) => {
+    var itemData = [
+      { "time": "08:00", "value": 1, "category": "kubernetes-" + kubernetes.name },
+      { "time": "08:30", "value": 5, "category": "kubernetes-" + kubernetes.name },
+      { "time": "09:00", "value": 5, "category": "kubernetes-" + kubernetes.name },
+      { "time": "09:30", "value": 1, "category": "kubernetes-" + kubernetes.name },
+      { "time": "10:00", "value": 5, "category": "kubernetes-" + kubernetes.name },
+      { "time": "10:30", "value": 2, "category": "kubernetes-" + kubernetes.name },
+      { "time": "11:00", "value": 5, "category": "kubernetes-" + kubernetes.name },
+      { "time": "11:30", "value": 10, "category": "kubernetes-" + kubernetes.name },
+    ];
+    cpuKubernetesData = cpuKubernetesData.concat(itemData);
   });
 
-  const data = vmData.concat(rdsData);
+  const cpuData = cpuVmData.concat(cpuRdsData).concat(cpuKafkaData).concat(cpuKubernetesData);
 
-  const config = {
-    data,
-    xField: 'time',
-    yField: 'value',
-    seriesField: 'category',
-  };
+  const logColumns = [
+    { title: 'Timestamp', dataIndex: 'timestamp', key: 'timestamp' },
+    { title: 'Location', dataIndex: 'loc', key: 'loc' },
+    { title: 'Level', dataIndex: 'level', key: 'level' },
+    { title: 'Message', dataIndex: 'message', key: 'message' }
+  ];
 
 	return (
 		<>
@@ -42,19 +87,21 @@ function Monitoring(props) {
 			<Row>
 				<Col span={12}>
 					<Card title="CPU Utilization (%)" bordered={true}>
-						<Line {...config} />
+						<Line data={cpuData} xField='time' yField='value' seriesField='category' />
 					</Card>
-					// Status check failed (count)
 				</Col>
 				<Col span={12}>
 					<Card title="Network in (bytes)" bordered={true}>
-						<Line {...config} />
+						<Line data={cpuData} xField='time' yField='value' seriesField='category' />
 					</Card>
-					// network out (bytes)
 				</Col>
-				// disk reads (bytes)
-				// dist writes (bytes)
 			</Row>
+      <Row>
+        <Col span={24}>
+          <Divider orientation="left">Logging</Divider>
+          <Table columns={logColumns} dataSource={logData} />
+        </Col>
+      </Row>
 		</>
 	);
 }
