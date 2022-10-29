5181f00b600b8219fa48308ca881aa6220dbf55f
XieYichao <xieyichao@outlook.com>
2022-10-29 16:35:34 +0800

    Refine the parser.

--- services/ocl-loader/src/main/java/org/eclipse/osc/services/ocl/loader/Ocl.java
+++ services/ocl-loader/src/main/java/org/eclipse/osc/services/ocl/loader/Ocl.java
@@ -2,16 +2,18 @@ package org.eclipse.osc.services.ocl.loader;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import lombok.extern.java.Log;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.logging.Level;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Log
 @Data
 public class Ocl {
 
@@ -31,37 +33,34 @@ public class Ocl {
     public <T> T referTo(String jsonPath, Class<T> valueType) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 
         if (!jsonPath.startsWith("$.")) {
-            System.out.println("Not a valid JsonPath.");
+            log.log(Level.WARNING, jsonPath + "is not a valid JsonPath.");
             throw new IllegalAccessException(jsonPath + "is not a valid JsonPath.");
         }
 
-        Matcher jsonPathMatter = Pattern.compile("([A-Za-z_0-9]+(?=[$\\.\\[\\]]{1}))").matcher(jsonPath);
+        Matcher matcher = Pattern.compile("([A-Za-z_0-9]+(?=[$\\.\\[\\]]{1}))").matcher(jsonPath);
 
-        List<String> matches = new ArrayList<String>();
-        while(jsonPathMatter.find()) {
-            matches.add(jsonPathMatter.group());
-        }
-
-        Object node = this;
-        for (String path: matches) {
-            if (path.equals("$")) {
+        Object object = this;
+        while(matcher.find()) {
+            String matchStr = matcher.group();
+            if (matchStr.equals("$")) {
                 continue;
             }
 
             try {
-                Integer index = Integer.parseInt(path);
-                Method getter = node.getClass().getDeclaredMethod("get", int.class);
-                node = getter.invoke(node, index);
+                Integer index = Integer.parseInt(matchStr);
+                Method getter = object.getClass().getDeclaredMethod("get", int.class);
+                object = getter.invoke(object, index);
             } catch (NumberFormatException e){
-                Method getter = node.getClass().getDeclaredMethod(
-                        "get" + path.substring(0,1).toUpperCase(Locale.ROOT) + path.substring(1));
-                node = getter.invoke(node);
+                Method getter = object.getClass().getDeclaredMethod("get" +
+                        matchStr.substring(0,1).toUpperCase(Locale.ROOT) + matchStr.substring(1));
+                object = getter.invoke(object);
             }
         }
 
-        if (node.getClass() == valueType) {
-            return (T) node;
+        if (object.getClass() == valueType) {
+            return (T) object;
         } else {
+            log.log(Level.WARNING, "Not the same type. Please check your JsonPath.");
             throw new IllegalAccessException("Not the same type. Please check your JsonPath.");
         }
     }
