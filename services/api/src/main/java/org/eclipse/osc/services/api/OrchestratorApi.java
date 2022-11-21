3b9e0b335bc9098c1c51fc16087ebe58433b2fa6
Jean-Baptiste Onofré <jbonofre@apache.org>
2022-11-21 09:39:01 +0100

    [#37] Create OSC simple REST API (#55)

---
+++ services/api/src/main/java/org/eclipse/osc/services/api/OrchestratorApi.java
@@ -0,0 +1,83 @@
+package org.eclipse.osc.services.api;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.karaf.minho.boot.Minho;
+import org.eclipse.osc.orchestrator.OrchestratorService;
+import org.eclipse.osc.services.ocl.loader.Ocl;
+
+@Slf4j
+@Path("/")
+public class OrchestratorApi {
+
+    @Path("/register")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response register(Ocl ocl) throws Exception {
+        getOrchestrator().registerManagedService(ocl);
+        return Response.ok().build();
+    }
+
+    @Path("/register/fetch")
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response fetch(@HeaderParam("ocl") String oclLocation) throws Exception {
+        getOrchestrator().registerManagedService(oclLocation);
+        return Response.ok().build();
+    }
+
+    @Path("/health")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String health() throws Exception {
+        if (getOrchestrator() == null) {
+            throw new IllegalStateException("Orchestrator service is not ready");
+        }
+        return "ready";
+    }
+
+    @Path("/start")
+    @POST
+    public Response start(@HeaderParam("managedServiceName") String managedServiceName) throws Exception {
+        getOrchestrator().startManagedService(managedServiceName);
+        return Response.ok().build();
+    }
+
+    @Path("/stop")
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response stop(@HeaderParam("managedServiceName") String managedServiceName) throws Exception {
+        getOrchestrator().stopManagedService(managedServiceName);
+        return Response.ok().build();
+    }
+
+    @Path("/update")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response update(@HeaderParam("managedServiceName") String managedServiceName, Ocl ocl) throws Exception {
+        getOrchestrator().updateManagedService(managedServiceName, ocl);
+        return Response.ok().build();
+    }
+
+    @Path("/update/fetch")
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response update(@HeaderParam("managedServiceName") String managedServiceName, String oclLocation) throws Exception {
+        getOrchestrator().updateManagedService(managedServiceName, oclLocation);
+        return Response.ok().build();
+    }
+
+    private OrchestratorService getOrchestrator() throws Exception {
+        Minho minho = Minho.getInstance();
+        OrchestratorService orchestratorService = minho.getServiceRegistry().get(OrchestratorService.class);
+
+        if (orchestratorService == null) {
+            throw new IllegalStateException("Orchestrator service is not available");
+        }
+
+        return orchestratorService;
+    }
+
+}
--- services/orchestrator/src/main/java/org/eclipse/osc/orchestrator/OrchestratorService.java
+++ services/orchestrator/src/main/java/org/eclipse/osc/orchestrator/OrchestratorService.java
@@ -51,13 +51,23 @@ public class OrchestratorService implements Service {
     }
 
     /**
-     * Register a managed service on all orchestrator plugins.
+     * Register a managed service on all orchestrator plugins, using OCL descriptor location.
      *
      * @param oclLocation the location of the OCL descriptor.
      * @throws Exception if registration fails.
      */
     public void registerManagedService(String oclLocation) throws Exception {
         Ocl ocl = oclLoader.getOcl(new URL(oclLocation));
+        registerManagedService(ocl);
+    }
+
+    /**
+     * Register a managed service on all orchestrator plugins, directly using OCL descriptor.
+     *
+     * @param ocl the OCL descriptor.
+     * @throws Exception if registration fails.
+     */
+    public void registerManagedService(Ocl ocl) throws Exception {
         if (ocl.getName() == null) {
             throw new IllegalArgumentException("Managed service name is required");
         }
@@ -68,14 +78,25 @@ public class OrchestratorService implements Service {
     }
 
     /**
-     * Update existing managed service with a new OCL descriptor.
+     * Update existing managed service with a new/updated OCL descriptor, at the given location
      *
      * @param managedServiceName the managed service to update, identified by the given name.
-     * @param oclLocation the new OCL descriptor.
+     * @param oclLocation the new/updated OCL descriptor location.
      * @throws Exception if the update fails.
      */
     public void updateManagedService(String managedServiceName, String oclLocation) throws Exception {
         Ocl ocl = oclLoader.getOcl(new URL(oclLocation));
+        updateManagedService(managedServiceName, ocl);
+    }
+
+    /**
+     * Update existing managed service with a new/updated OCL descriptor.
+     *
+     * @param managedServiceName the managed service to update, identified by the given name.
+     * @param ocl the new/update OCL descriptor.
+     * @throws Exception if the update fails.
+     */
+    public void updateManagedService(String managedServiceName, Ocl ocl) throws Exception {
         plugins.forEach(plugin -> {
             plugin.updateManagedService(managedServiceName, ocl);
         });
