e93f00a7078d6938e52f2221cd9425bafd60501d
Swaroop Akulu Raghupathy <s00828547@china.huawei.com>
2023-01-05 15:12:37 +0100

    add new module to configure logging

---
+++ services/logging/src/main/java/org/eclipse/osc/services/logging/LogFormatConfiguration.java
@@ -0,0 +1,28 @@
+package org.eclipse.osc.services.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.karaf.minho.boot.service.ConfigService;
+import org.apache.karaf.minho.boot.service.ServiceRegistry;
+import org.apache.karaf.minho.boot.spi.Service;
+
+import java.util.Objects;
+
+@Slf4j
+public class LogFormatConfiguration implements Service {
+
+    @Override
+    public void onRegister(ServiceRegistry serviceRegistry) {
+        log.info("Registering log configuration");
+        if (Objects.isNull(serviceRegistry)) {
+            throw new IllegalStateException("ServiceRegistry is null");
+        }
+        ConfigService configService = serviceRegistry.get(ConfigService.class);
+        if (Objects.isNull(configService)) {
+            throw new IllegalStateException("Config service is not present in the registry");
+        }
+        if (Objects.nonNull(configService.getProperty("jul.log.format"))) {
+            System.setProperty("java.util.logging.SimpleFormatter.format",
+                    configService.getProperty("jul.log.format"));
+        }
+    }
+}
