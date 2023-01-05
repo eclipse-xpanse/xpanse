72ec7ba5e52a8ab81fca751e60ae43d206acacbc
Swaroop Akulu Raghupathy <s00828547@china.huawei.com>
2023-01-05 15:16:30 +0100

    add main class

---
+++ runtime/src/main/java/org/eclipse/osc/runtime/OscMain.java
@@ -0,0 +1,10 @@
+package org.eclipse.osc.runtime;
+
+import org.apache.karaf.minho.boot.Main;
+
+public class OscMain {
+
+    public static void main(String[] args) throws Exception {
+        Main.main(args);
+    }
+}
