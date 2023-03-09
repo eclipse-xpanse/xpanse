3ca9fd6b1b571cd2d8a8401f267565d0aaf78423
swaroopar <swaroop.akulu.raghupathy@huawei.com>
2023-03-09 12:30:10 +0100

    display register panel only for csp user

--- ui/src/components/layouts/sider/LayoutSider.tsx
+++ ui/src/components/layouts/sider/LayoutSider.tsx
@@ -7,11 +7,18 @@ import { Image, Menu } from 'antd';
 import Sider from 'antd/es/layout/Sider';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { homePageRoute } from '../../utils/constants';
+import { homePageRoute, usernameKey } from '../../utils/constants';
 import registerPanelMenu from '../../content/register/registerPanelMenu';
+import { MenuItemType } from 'antd/lib/menu/hooks/useItems';
 
 function LayoutSider(): JSX.Element {
     const [collapsed, setCollapsed] = useState(false);
+    const menuItems: MenuItemType[] = [];
+
+    if (localStorage.getItem(usernameKey) === 'csp') {
+        menuItems.push(registerPanelMenu());
+    }
+
     return (
         <Sider collapsible collapsed={collapsed} onCollapse={(newValue) => setCollapsed(newValue)}>
             <div className='logo'>
@@ -19,7 +26,7 @@ function LayoutSider(): JSX.Element {
                     <Image width={50} src='./logo.png' preview={false} />
                 </Link>
             </div>
-            <Menu items={[registerPanelMenu()]} mode='inline' theme='dark' />
+            <Menu items={menuItems} mode='inline' theme='dark' />
         </Sider>
     );
 }
