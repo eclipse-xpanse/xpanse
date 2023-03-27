da9fbe7d55258825ef491917b96d4bcf6c3f594f
jinyangyang222 <jinyangyang222@outlook.com>
2023-03-27 10:54:10 +0800

    deployment test and openApi config

--- modules/deployment/src/test/java/org/eclipse/xpanse/modules/deployment/deployers/terraform/TerraformDeploymentTest.java
+++ modules/deployment/src/test/java/org/eclipse/xpanse/modules/deployment/deployers/terraform/TerraformDeploymentTest.java
@@ -18,6 +18,7 @@ import org.eclipse.xpanse.modules.models.service.CreateRequest;
 import org.eclipse.xpanse.modules.models.service.DeployResult;
 import org.eclipse.xpanse.modules.models.utils.OclLoader;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -26,31 +27,36 @@ import org.junit.jupiter.api.Test;
 @Slf4j
 public class TerraformDeploymentTest {
 
+    private static OclLoader oclLoader;
+    private static CreateRequest createRequest;
+    private static DeployTask deployTask;
     private final TerraformDeployment terraformDeployment = new TerraformDeployment();
 
-    @Test
-    public void deploy_test() throws Exception {
-        OclLoader oclLoader = new OclLoader();
+    @BeforeAll
+    public static void initData() throws Exception {
+        oclLoader = new OclLoader();
         Ocl ocl = oclLoader.getOcl(new URL("file:./target/test-classes/ocl_test.yaml"));
 
-        CreateRequest createRequest = new CreateRequest();
+        createRequest = new CreateRequest();
         createRequest.setName(ocl.getName());
         createRequest.setCsp(ocl.getCloudServiceProvider().getName());
         createRequest.setVersion(ocl.getVersion());
         createRequest.setFlavor(ocl.getFlavors().get(0).getName());
         createRequest.setRegion(ocl.getCloudServiceProvider().getRegions().get(0));
-
         Map<String, String> property = new HashMap<>();
         // The secgroup_id use already exists.
         property.put("secgroup_id", "e2d4de73-1518-40f7-8de1-60f184ea6e1d");
         createRequest.setProperty(property);
 
-        DeployTask deployTask = new DeployTask();
+        deployTask = new DeployTask();
         deployTask.setId(UUID.randomUUID());
         deployTask.setCreateRequest(createRequest);
         deployTask.setOcl(ocl);
         deployTask.setDeployResourceHandler(null);
+    }
 
+    @Test
+    public void deploy_test() throws Exception {
         DeployResult deployResult = terraformDeployment.deploy(deployTask);
         log.error(deployResult.getProperty().get("stateFile"));
         Assertions.assertEquals(TerraformExecState.DEPLOY_SUCCESS, deployResult.getState());
@@ -58,28 +64,8 @@ public class TerraformDeploymentTest {
 
     @Test
     public void destroy_test() throws Exception {
-        OclLoader oclLoader = new OclLoader();
-        Ocl ocl = oclLoader.getOcl(new URL("file:./target/test-classes/ocl_test.yaml"));
-
-        CreateRequest createRequest = new CreateRequest();
-        createRequest.setName(ocl.getName());
-        createRequest.setCsp(ocl.getCloudServiceProvider().getName());
-        createRequest.setVersion(ocl.getVersion());
-        createRequest.setFlavor(ocl.getFlavors().get(0).getName());
-        createRequest.setRegion(ocl.getCloudServiceProvider().getRegions().get(0));
-
-        Map<String, String> property = new HashMap<>();
-        // The secgroup_id usage already exists.
-        property.put("secgroup_id", "e2d4de73-1518-40f7-8de1-60f184ea6e1d");
-        createRequest.setProperty(property);
-
-        DeployTask deployTask = new DeployTask();
         // The tastId use the resource ID that has been successfully deployed.
-        deployTask.setId(UUID.fromString("c2366707-1844-4234-b27a-c5a96dc56617"));
-        deployTask.setCreateRequest(createRequest);
-        deployTask.setOcl(ocl);
-        deployTask.setDeployResourceHandler(null);
-
+        deployTask.setId(UUID.fromString("b4c10f12-7d49-43c7-951f-04d2509c4dd7"));
         DeployResult deployResult = terraformDeployment.destroy(deployTask);
         Assertions.assertEquals(TerraformExecState.DESTROY_SUCCESS, deployResult.getState());
     }
