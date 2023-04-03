6fbe02079f3c00d29457d820fea8c594608dc10b
yichaoxie <xieyichao@huawei.com>
2023-04-03 13:02:22 +0800

    Add some deploy variable kinds.

---
+++ modules/deployment/src/main/java/org/eclipse/xpanse/modules/deployment/utils/DeployEnvironments.java
@@ -0,0 +1,105 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * SPDX-FileCopyrightText: Huawei Inc.
+ *
+ */
+
+package org.eclipse.xpanse.modules.deployment.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.eclipse.xpanse.modules.deployment.deployers.terraform.DeployTask;
+import org.eclipse.xpanse.modules.models.enums.DeployVariableKind;
+import org.eclipse.xpanse.modules.models.resource.DeployVariable;
+import org.eclipse.xpanse.modules.models.resource.Flavor;
+
+/**
+ * Environment variables utils for deployment.
+ */
+public class DeployEnvironments {
+
+    private DeployEnvironments() {
+    }
+
+    /**
+     * Get environment variables for deployment.
+     *
+     * @param task the context of the task.
+     */
+    public static Map<String, String> getEnv(DeployTask task) {
+        Map<String, String> variables = new HashMap<>();
+        Map<String, String> request = task.getCreateRequest().getProperty();
+        for (DeployVariable variable : task.getOcl().getDeployment().getContext()) {
+            if (variable.getKind() == DeployVariableKind.ENV) {
+                if (request.containsKey(variable.getName())
+                        && request.get(variable.getName()) != null) {
+                    variables.put(variable.getName(), request.get(variable.getName()));
+                } else {
+                    variables.put(variable.getName(), System.getenv(variable.getName()));
+                }
+            }
+
+            if (variable.getKind() == DeployVariableKind.ENV_ENV) {
+                variables.put(variable.getName(), System.getenv(variable.getName()));
+            }
+
+            if (variable.getKind() == DeployVariableKind.FIX_ENV) {
+                variables.put(variable.getName(), variable.getValue());
+            }
+        }
+
+        return variables;
+    }
+
+    /**
+     * Get flavor variables.
+     *
+     * @param task the DeployTask.
+     */
+    public static Map<String, String> getFlavorVariables(DeployTask task) {
+        Map<String, String> variables = new HashMap<>();
+
+        variables.put("flavor", task.getCreateRequest().getFlavor());
+        for (Flavor flavor : task.getOcl().getFlavors()) {
+            if (flavor.getName().equals(task.getCreateRequest().getFlavor())) {
+                for (Map.Entry<String, String> entry : flavor.getProperty().entrySet()) {
+                    variables.put(("flavor_" + entry.getKey()), entry.getValue());
+                }
+                return variables;
+            }
+        }
+
+        throw new RuntimeException("Can not get an available flavor.");
+    }
+
+    /**
+     * Get deployment variables.
+     *
+     * @param task the DeployTask.
+     */
+    public static Map<String, String> getVariables(DeployTask task) {
+        Map<String, String> variables = new HashMap<>();
+        Map<String, String> request = task.getCreateRequest().getProperty();
+        for (DeployVariable variable : task.getOcl().getDeployment().getContext()) {
+            if (variable.getKind() == DeployVariableKind.VARIABLE) {
+                if (request.containsKey(variable.getName())
+                        && request.get(variable.getName()) != null) {
+                    variables.put(variable.getName(), request.get(variable.getName()));
+                } else {
+                    variables.put(variable.getName(), System.getenv(variable.getName()));
+                }
+            }
+
+            if (variable.getKind() == DeployVariableKind.ENV_VARIABLE) {
+                variables.put(variable.getName(), System.getenv(variable.getName()));
+            }
+
+            if (variable.getKind() == DeployVariableKind.FIX_VARIABLE
+                    && request.containsKey(variable.getName())) {
+                variables.put(variable.getName(), variable.getValue());
+            }
+        }
+
+        return variables;
+    }
+}
--- modules/models/src/main/java/org/eclipse/xpanse/modules/models/enums/DeployVariableKind.java
+++ modules/models/src/main/java/org/eclipse/xpanse/modules/models/enums/DeployVariableKind.java
@@ -12,12 +12,28 @@ import org.apache.commons.lang3.StringUtils;
 
 /**
  * Deploy variable kinds.
+ * fix_env: This kind of variables must be settled in the OCL template,
+ *   and will be injected to the deployment as environment variables.
+ * fix_variable: This kind of variables must be settled in the OCL template,
+ *   and will be injected to the deployment as variables.
+ * env: This kind of variables can be settled by the API or the xpanse
+ *   runtime environment (The API takes higher priority), and will be injected
+ *   to the deployment as environment variables.
+ * variable: This kind of variables can be settled by the API or the xpanse
+ *   runtime environment (The API takes higher priority), and will be injected
+ *   to the deployment as variables.
+ * env_env: This kind of variables must be settled by the xpanse runtime environment,
+ *   and will be injected to the deployment as environment variables.
+ * env_variable: This kind of variables must be settled by the xpanse runtime
+ *   environment, and will be injected to the deployment as variables.
  */
 public enum DeployVariableKind {
     FIX_ENV("fix_env"),
     FIX_VARIABLE("fix_variable"),
     ENV("env"),
-    VARIABLE("variable");
+    VARIABLE("variable"),
+    ENV_ENV("env_env"),
+    ENV_VARIABLE("env_variable");
 
     private final String type;
 
