f374d8cc10efec9667e1c9dedaf23a3ccd49f958
swaroopar <swaroop.akulu.raghupathy@huawei.com>
2023-04-26 14:44:13 +0200

    log HTTP request and responses for tracing

---
+++ modules/logging/src/main/java/org/eclipse/xpanse/modules/logging/CustomRequestIdGenerator.java
@@ -0,0 +1,25 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * SPDX-FileCopyrightText: Huawei Inc.
+ */
+
+package org.eclipse.xpanse.modules.logging;
+
+import java.util.UUID;
+import org.slf4j.MDC;
+import org.springframework.lang.NonNull;
+import org.zalando.logbook.CorrelationId;
+import org.zalando.logbook.HttpRequest;
+
+/**
+ * Custom unique ID generated per request by Logbook.
+ */
+public class CustomRequestIdGenerator implements CorrelationId {
+
+    @Override
+    public String generate(@NonNull HttpRequest request) {
+        String uuid = UUID.randomUUID().toString();
+        MDC.put("TASK_ID", uuid);
+        return uuid;
+    }
+}
