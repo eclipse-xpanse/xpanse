957602e0f674df6f543128629191b50b66ba180e
swaroopar <swaroop.akulu.raghupathy@huawei.com>
2023-06-16 12:41:42 +0200

    refactor data model packages

--- modules/models/src/main/java/org/eclipse/xpanse/modules/models/response/ResultType.java
+++ modules/models/src/main/java/org/eclipse/xpanse/modules/models/response/ResultType.java
@@ -1,10 +1,9 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  * SPDX-FileCopyrightText: Huawei Inc.
- *
  */
 
-package org.eclipse.xpanse.api.response;
+package org.eclipse.xpanse.modules.models.response;
 
 
 import com.fasterxml.jackson.annotation.JsonCreator;
