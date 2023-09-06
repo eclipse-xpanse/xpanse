6784abd8523a153a58e2367fb52220b89f7b107f
WangLiNaruto <wl-naruto@outlook.com>
2023-09-06 16:31:00 +0800

    remove port hard coding in the webhook url

--- modules/deployment/src/main/java/org/eclipse/xpanse/modules/deployment/deployers/terraform/config/TerraformBootConfig.java
+++ modules/deployment/src/main/java/org/eclipse/xpanse/modules/deployment/deployers/terraform/config/TerraformBootConfig.java
@@ -6,11 +6,7 @@
 
 package org.eclipse.xpanse.modules.deployment.deployers.terraform.config;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import lombok.Getter;
-import org.apache.commons.lang3.StringUtils;
-import org.eclipse.xpanse.modules.models.service.deploy.exceptions.TerraformBootRequestFailedException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -34,18 +30,4 @@ public class TerraformBootConfig {
     @Value("${webhook.destroyCallbackUri}")
     private String destroyCallbackUri;
 
-    /**
-     * Get the hostname of Xpanse service.
-     */
-    public String getClientRequestBaseUrl() throws TerraformBootRequestFailedException {
-        try {
-            if (StringUtils.isBlank(clientBaseUri)) {
-                return String.format("http://%s:8080", InetAddress.getLocalHost().getHostAddress());
-            } else {
-                return clientBaseUri;
-            }
-        } catch (UnknownHostException e) {
-            throw new TerraformBootRequestFailedException(e.getMessage());
-        }
-    }
 }
