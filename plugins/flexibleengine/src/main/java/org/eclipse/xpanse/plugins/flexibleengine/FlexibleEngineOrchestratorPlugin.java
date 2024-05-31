0f53ec967c5e24189748045520a168839a3ff945
WangLiNaruto <122504203+WangLiNaruto@users.noreply.github.com>
2024-05-31 15:51:10 +0800

    add caching for getAvailabilityZones (#1658)

--- plugins/flexibleengine/src/main/java/org/eclipse/xpanse/plugins/flexibleengine/FlexibleEngineOrchestratorPlugin.java
+++ plugins/flexibleengine/src/main/java/org/eclipse/xpanse/plugins/flexibleengine/FlexibleEngineOrchestratorPlugin.java
@@ -13,6 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.xpanse.modules.cache.consts.CacheNames;
 import org.eclipse.xpanse.modules.models.billing.FlavorPriceResult;
 import org.eclipse.xpanse.modules.models.common.enums.Csp;
 import org.eclipse.xpanse.modules.models.credential.AbstractCredentialInfo;
@@ -36,6 +37,7 @@ import org.eclipse.xpanse.plugins.flexibleengine.monitor.FlexibleEngineMetricsSe
 import org.eclipse.xpanse.plugins.flexibleengine.price.FlexibleEnginePriceCalculator;
 import org.eclipse.xpanse.plugins.flexibleengine.resourcehandler.FlexibleEngineTerraformResourceHandler;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 /**
@@ -72,6 +74,7 @@ public class FlexibleEngineOrchestratorPlugin implements OrchestratorPlugin {
     }
 
     @Override
+    @Cacheable(CacheNames.REGION_AZ_CACHE_NAME)
     public List<String> getAvailabilityZonesOfRegion(String userId, String region) {
         return flexibleEngineResourceManager.getAvailabilityZonesOfRegion(userId, region);
     }
--- runtime/src/main/java/org/eclipse/xpanse/runtime/XpanseApplication.java
+++ runtime/src/main/java/org/eclipse/xpanse/runtime/XpanseApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -21,6 +22,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 /**
  * Main entry class to xpanse runtime. This class can be directly executed to start the server.
  */
+@EnableCaching
 @EnableRetry
 @EnableAsync(proxyTargetClass = true)
 @EnableJpaRepositories(basePackages = "org.eclipse.xpanse")
