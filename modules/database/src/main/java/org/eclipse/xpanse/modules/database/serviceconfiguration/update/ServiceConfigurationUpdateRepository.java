77a9b4a68e09b8829dde6cf07c0d20d787703642
WangLiNaruto <wl-naruto@outlook.com>
2024-09-24 16:48:00 +0800

    add distributed locking mechanism for requests from agents

--- modules/database/src/main/java/org/eclipse/xpanse/modules/database/serviceconfiguration/update/ServiceConfigurationUpdateRepository.java
+++ modules/database/src/main/java/org/eclipse/xpanse/modules/database/serviceconfiguration/update/ServiceConfigurationUpdateRepository.java
@@ -6,9 +6,16 @@
 
 package org.eclipse.xpanse.modules.database.serviceconfiguration.update;
 
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
+import java.util.List;
 import java.util.UUID;
 import org.eclipse.xpanse.modules.database.CustomJpaRepository;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.lang.Nullable;
 
 /**
  * Interface to access default JPA methods.
@@ -17,4 +24,10 @@ public interface ServiceConfigurationUpdateRepository extends
         CustomJpaRepository<ServiceConfigurationUpdateRequest, UUID>,
         JpaSpecificationExecutor<ServiceConfigurationUpdateRequest> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    // it is not possible to read this config from spring properties.
+    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value = "2000")})
+    @Override
+    List<ServiceConfigurationUpdateRequest> findAll(
+            @Nullable Specification<ServiceConfigurationUpdateRequest> spec);
 }
