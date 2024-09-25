afbe4266ae1d7507a14e068de1a02a9c3e6a1056
swaroopar <swaroop.akulu.raghupathy@huawei.com>
2024-09-25 12:51:30 +0000

    add no content response type to openapi doc

--- modules/api/src/main/java/org/eclipse/xpanse/api/controllers/AgentPollingApi.java
+++ modules/api/src/main/java/org/eclipse/xpanse/api/controllers/AgentPollingApi.java
@@ -8,20 +8,22 @@ package org.eclipse.xpanse.api.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.xpanse.api.config.AuditApiRequest;
 import org.eclipse.xpanse.modules.deployment.ServiceConfigurationManager;
 import org.eclipse.xpanse.modules.models.serviceconfiguration.ServiceConfigurationChangeRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -40,9 +42,18 @@ public class AgentPollingApi {
             description = "APIs for agent to poll pending configuration change requests.")
     @GetMapping(value = "/agent/poll/{serviceId}/{resourceName}",
             produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseStatus(HttpStatus.OK)
     @Operation(description = "Get pending configuration change request for agents to poll.")
     @AuditApiRequest(enabled = false)
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "no pending configuration update requests",
+                    content = @Content),
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "pending configuration update request details",
+                    content = @Content(schema = @Schema(
+                    implementation = ServiceConfigurationChangeRequest.class)))})
     public ResponseEntity<ServiceConfigurationChangeRequest> getPendingConfigurationChangeRequest(
             @Parameter(name = "serviceId",
                     description = "The id of the deployed service")
