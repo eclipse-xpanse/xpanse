2cfc54c15807e15f54fd16e4556eba708c03c16a
baixinsui <baixinsui@outlook.com>
2024-12-18 17:18:36 +0800

    Add new register request history when updating un-approved service templates

--- modules/database/src/main/java/org/eclipse/xpanse/modules/database/enumcolumn/EnumColumnConstraintManage.java
+++ modules/database/src/main/java/org/eclipse/xpanse/modules/database/enumcolumn/EnumColumnConstraintManage.java
@@ -8,7 +8,6 @@ package org.eclipse.xpanse.modules.database.enumcolumn;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -57,51 +56,18 @@ public class EnumColumnConstraintManage {
     }
 
     /**
-     * Query the constraints of enum column in table for H2.
+     * Update the enum column values in table for H2.
      *
      * @param tableName table name.
      * @param columnName column name.
-     * @return the constraints of enum column in table for H2.
-     */
-    @SuppressWarnings("unchecked")
-    public List<String> queryConstraintForH2TableEnumColumn(String tableName, String columnName) {
-        String queryStatement =
-                String.format(
-                        "SELECT T2.CONSTRAINT_NAME FROM INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE"
-                            + " T1, INFORMATION_SCHEMA.CHECK_CONSTRAINTS T2  WHERE"
-                            + " T1.CONSTRAINT_NAME = T2.CONSTRAINT_NAME  AND TABLE_NAME = '%s' AND"
-                            + " COLUMN_NAME = '%s'",
-                        tableName, columnName);
-        return entityManager.createNativeQuery(queryStatement).getResultList();
-    }
-
-    /**
-     * Drop the old constraint of enum column in table for H2.
-     *
-     * @param tableName table name.
-     * @param constraintName constraint name.
-     */
-    public void dropOldConstraintForH2TableEnumColumn(String tableName, String constraintName) {
-        String ddlStatement =
-                String.format("ALTER TABLE %s DROP CONSTRAINT %s", tableName, constraintName);
-        entityManager.createNativeQuery(ddlStatement).executeUpdate();
-    }
-
-    /**
-     * Add the new constraint of enum column in table for H2.
-     *
-     * @param tableName table name.
-     * @param columnName column name.
-     * @param constraintName constraint name.
      * @param enumValuesString the string of enum values.
      */
-    public void addNewConstraintForH2TableEnumColumn(
-            String tableName, String columnName, String constraintName, String enumValuesString) {
-        String constraintValue = String.format("\"%s\" IN (%s)", columnName, enumValuesString);
+    public void updateTableEnumColumnValuesForH2(
+            String tableName, String columnName, String enumValuesString) {
         String ddlStatement =
                 String.format(
-                        "ALTER TABLE %s ADD CONSTRAINT %s CHECK(%s)",
-                        tableName, constraintName, constraintValue);
+                        "ALTER TABLE %s ALTER COLUMN %s ENUM(%s)",
+                        tableName, columnName, enumValuesString);
         entityManager.createNativeQuery(ddlStatement).executeUpdate();
     }
 }
