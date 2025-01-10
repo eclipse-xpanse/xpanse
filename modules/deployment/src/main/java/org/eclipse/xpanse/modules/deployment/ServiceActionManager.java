2854de62971b3a4836a0f69b883a700aa5892d2b
WangLiNaruto <122504203+WangLiNaruto@users.noreply.github.com>
2025-01-10 22:30:53 +0800

    add service actions API (#2286)

---
+++ modules/deployment/src/main/java/org/eclipse/xpanse/modules/deployment/ServiceActionManager.java
@@ -0,0 +1,158 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * SPDX-FileCopyrightText: Huawei Inc.
+ *
+ */
+
+package org.eclipse.xpanse.modules.deployment;
+
+import jakarta.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.xpanse.modules.database.service.ServiceDeploymentEntity;
+import org.eclipse.xpanse.modules.database.serviceconfiguration.update.ServiceChangeDetailsEntity;
+import org.eclipse.xpanse.modules.database.serviceconfiguration.update.ServiceChangeDetailsStorage;
+import org.eclipse.xpanse.modules.database.servicetemplate.ServiceTemplateEntity;
+import org.eclipse.xpanse.modules.database.servicetemplate.ServiceTemplateStorage;
+import org.eclipse.xpanse.modules.logging.CustomRequestIdGenerator;
+import org.eclipse.xpanse.modules.models.service.deploy.DeployResource;
+import org.eclipse.xpanse.modules.models.service.enums.DeployResourceKind;
+import org.eclipse.xpanse.modules.models.service.order.ServiceOrder;
+import org.eclipse.xpanse.modules.models.service.order.enums.ServiceOrderType;
+import org.eclipse.xpanse.modules.models.service.utils.ServiceConfigurationVariablesJsonSchemaGenerator;
+import org.eclipse.xpanse.modules.models.service.utils.ServiceConfigurationVariablesJsonSchemaValidator;
+import org.eclipse.xpanse.modules.models.serviceaction.ServiceActionRequest;
+import org.eclipse.xpanse.modules.models.serviceconfiguration.exceptions.ServiceConfigurationInvalidException;
+import org.eclipse.xpanse.modules.models.servicetemplate.Ocl;
+import org.eclipse.xpanse.modules.models.servicetemplate.ServiceChangeParameter;
+import org.eclipse.xpanse.modules.models.servicetemplate.ServiceChangeScript;
+import org.eclipse.xpanse.modules.models.servicetemplate.exceptions.ServiceTemplateNotRegistered;
+import org.eclipse.xpanse.modules.models.servicetemplate.utils.JsonObjectSchema;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+/** Bean to manage service actions. */
+@Slf4j
+@Component
+public class ServiceActionManager {
+
+    @Resource private ServiceDeploymentEntityHandler serviceDeploymentEntityHandler;
+
+    @Resource private ServiceChangeDetailsStorage serviceChangeDetailsStorage;
+
+    @Resource private ServiceTemplateStorage serviceTemplateStorage;
+
+    @Resource private DeployService deployService;
+
+    @Resource private ServiceChangeDetailsManager serviceChangeDetailsManager;
+
+    @Resource
+    private ServiceConfigurationVariablesJsonSchemaValidator
+            serviceConfigurationVariablesJsonSchemaValidator;
+
+    @Resource
+    private ServiceConfigurationVariablesJsonSchemaGenerator
+            serviceConfigurationVariablesJsonSchemaGenerator;
+
+    /** create service action. */
+    public ServiceOrder createServiceAction(String serviceId, ServiceActionRequest request) {
+        try {
+            ServiceDeploymentEntity serviceDeploymentEntity =
+                    serviceDeploymentEntityHandler.getServiceDeploymentEntity(
+                            UUID.fromString(serviceId));
+            ServiceTemplateEntity serviceTemplateEntity =
+                    serviceTemplateStorage.getServiceTemplateById(
+                            serviceDeploymentEntity.getServiceTemplateId());
+            if (Objects.isNull(serviceTemplateEntity)) {
+                String errMsg =
+                        String.format(
+                                "Service template with id %s not found.",
+                                serviceDeploymentEntity.getServiceTemplateId());
+                log.error(errMsg);
+                throw new ServiceTemplateNotRegistered(errMsg);
+            }
+            validateServiceActions(serviceTemplateEntity, request.getActionParameters());
+            UUID orderId = CustomRequestIdGenerator.generateOrderId();
+            addServiceChangeDetailsForServiceActions(
+                    orderId,
+                    serviceId,
+                    serviceDeploymentEntity,
+                    serviceTemplateEntity.getOcl(),
+                    request.getActionParameters(),
+                    request.getActionName());
+            return new ServiceOrder(orderId, UUID.fromString(serviceId));
+        } catch (ServiceConfigurationInvalidException e) {
+            String errorMsg =
+                    String.format("Change service configuration error, %s", e.getErrorReasons());
+            log.error(errorMsg);
+            throw e;
+        }
+    }
+
+    private void validateServiceActions(
+            ServiceTemplateEntity serviceTemplateEntity,
+            Map<String, Object> updateActionParameters) {
+        List<ServiceChangeParameter> actionParameters =
+                serviceTemplateEntity.getOcl().getServiceActions().stream()
+                        .flatMap(action -> action.getActionParameters().stream())
+                        .collect(Collectors.toList());
+        if (Objects.isNull(actionParameters)) {
+            String errorMsg =
+                    String.format(
+                            "Service template %s has no service actions manage",
+                            serviceTemplateEntity.getId());
+            log.error(errorMsg);
+            throw new ServiceConfigurationInvalidException(List.of(errorMsg));
+        }
+
+        JsonObjectSchema jsonObjectSchema =
+                serviceConfigurationVariablesJsonSchemaGenerator
+                        .buildServiceConfigurationJsonSchema(actionParameters);
+        serviceConfigurationVariablesJsonSchemaValidator.validateServiceConfiguration(
+                actionParameters, updateActionParameters, jsonObjectSchema);
+    }
+
+    private void addServiceChangeDetailsForServiceActions(
+            UUID orderId,
+            String serviceId,
+            ServiceDeploymentEntity serviceDeployment,
+            Ocl ocl,
+            Map<String, Object> updateRequestMap,
+            String actionName) {
+
+        Map<String, List<DeployResource>> deployResourceMap =
+                deployService
+                        .listResourcesOfDeployedService(
+                                UUID.fromString(serviceId), DeployResourceKind.VM)
+                        .stream()
+                        .collect(Collectors.groupingBy(DeployResource::getGroupName));
+
+        List<ServiceChangeScript> actionManageScripts =
+                ocl.getServiceActions().stream()
+                        .filter(serviceAction -> actionName.equals(serviceAction.getName()))
+                        .flatMap(action -> action.getActionManageScripts().stream())
+                        .toList();
+
+        List<ServiceChangeParameter> actionParameters =
+                ocl.getServiceActions().stream()
+                        .filter(serviceAction -> actionName.equals(serviceAction.getName()))
+                        .flatMap(action -> action.getActionParameters().stream())
+                        .toList();
+        List<ServiceChangeDetailsEntity> requests =
+                serviceChangeDetailsManager.getAllServiceChangeDetails(
+                        orderId,
+                        serviceDeployment,
+                        updateRequestMap,
+                        deployResourceMap,
+                        actionManageScripts,
+                        actionParameters,
+                        ServiceOrderType.SERVICE_ACTION);
+        if (!CollectionUtils.isEmpty(requests)) {
+            serviceChangeDetailsStorage.saveAll(requests);
+        }
+    }
+}
---
+++ modules/models/src/main/java/org/eclipse/xpanse/modules/models/serviceaction/ServiceActionRequest.java
@@ -0,0 +1,27 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * SPDX-FileCopyrightText: Huawei Inc.
+ */
+
+package org.eclipse.xpanse.modules.models.serviceaction;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.Map;
+import lombok.Data;
+
+/** ServiceAction request model. */
+@Data
+public class ServiceActionRequest {
+
+    @NotNull
+    @Size(min = 1)
+    @Schema(description = "The name service action.")
+    private String actionName;
+
+    @NotNull
+    @Size(min = 1)
+    @Schema(description = "The service actions parameter to be modified")
+    private Map<String, Object> actionParameters;
+}
