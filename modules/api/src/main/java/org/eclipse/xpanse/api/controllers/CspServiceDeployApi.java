6143b19392fc4ba19aef19c0d75d8ee4264c4f33
WangLiNaruto <wl-naruto@outlook.com>
2025-01-17 16:29:14 +0800

    Registered Services Page Called Error API for ISV user

--- modules/api/src/main/java/org/eclipse/xpanse/api/controllers/CspServiceDeployApi.java
+++ modules/api/src/main/java/org/eclipse/xpanse/api/controllers/CspServiceDeployApi.java
@@ -6,29 +6,25 @@
 
 package org.eclipse.xpanse.api.controllers;
 
-import static org.eclipse.xpanse.modules.security.common.RoleConstants.ROLE_ISV;
+import static org.eclipse.xpanse.modules.security.common.RoleConstants.ROLE_CSP;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import java.util.List;
-import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.xpanse.api.config.AuditApiRequest;
 import org.eclipse.xpanse.modules.deployment.ServiceDetailsViewManager;
 import org.eclipse.xpanse.modules.models.common.enums.Category;
-import org.eclipse.xpanse.modules.models.common.enums.Csp;
 import org.eclipse.xpanse.modules.models.service.enums.ServiceDeploymentState;
 import org.eclipse.xpanse.modules.models.service.view.DeployedService;
-import org.eclipse.xpanse.modules.models.service.view.DeployedServiceDetails;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -39,29 +35,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/xpanse")
 @CrossOrigin
-@Secured({ROLE_ISV})
+@Secured({ROLE_CSP})
 @ConditionalOnProperty(name = "enable.agent.api.only", havingValue = "false", matchIfMissing = true)
-public class IsvServiceDeployApi {
+public class CspServiceDeployApi {
 
     @Resource private ServiceDetailsViewManager serviceDetailsViewManager;
 
     /**
-     * List all deployed services by a user of ISV.
+     * List all deployed services by a user of CSP.
      *
      * @return list of all services deployed by a user.
      */
     @Tag(name = "Service", description = "APIs to manage the services")
-    @Operation(description = "List services of an ISV deployed by users. ")
-    @GetMapping(value = "/services/isv", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(description = "List of services deployed on a cloud provider.")
+    @GetMapping(value = "/services/csp", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     @AuditApiRequest(methodName = "getCspFromRequestUri")
-    public List<DeployedService> getAllDeployedServicesByIsv(
+    public List<DeployedService> getAllDeployedServicesByCsp(
             @Parameter(name = "categoryName", description = "category of the service")
                     @RequestParam(name = "categoryName", required = false)
                     Category category,
-            @Parameter(name = "cspName", description = "name of the cloud service provider")
-                    @RequestParam(name = "cspName", required = false)
-                    Csp csp,
             @Parameter(name = "serviceName", description = "name of the service")
                     @RequestParam(name = "serviceName", required = false)
                     String serviceName,
@@ -71,26 +64,7 @@ public class IsvServiceDeployApi {
             @Parameter(name = "serviceState", description = "deployment state of the service")
                     @RequestParam(name = "serviceState", required = false)
                     ServiceDeploymentState serviceState) {
-        return this.serviceDetailsViewManager.getAllDeployedServicesByIsv(
-                category, csp, serviceName, serviceVersion, serviceState);
-    }
-
-    /**
-     * Get details of the managed service by serviceId for ISV role.
-     *
-     * @return Details of the managed service.
-     */
-    @Tag(name = "Service", description = "APIs to manage the services")
-    @Operation(description = "Get the details of the deployed service by service id.")
-    @GetMapping(
-            value = "/services/isv/details/vendor_hosted/{serviceId}",
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseStatus(HttpStatus.OK)
-    @AuditApiRequest(methodName = "getCspFromServiceId", paramTypes = UUID.class)
-    public DeployedServiceDetails getServiceDetailsByIdForIsv(
-            @Parameter(name = "serviceId", description = "Id of deployed service")
-                    @PathVariable("serviceId")
-                    UUID serviceId) {
-        return this.serviceDetailsViewManager.getServiceDetailsByIdForIsv(serviceId);
+        return this.serviceDetailsViewManager.getAllDeployedServicesByCsp(
+                category, serviceName, serviceVersion, serviceState);
     }
 }
