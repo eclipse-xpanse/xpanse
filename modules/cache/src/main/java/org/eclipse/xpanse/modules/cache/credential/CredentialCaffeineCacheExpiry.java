b66cdbf3de6baa6ac4cea8046a29a139eab5be1d
swaroop.ar <swaroop.akulu.raghupathy@huawei.com>
2025-06-11 15:55:25 +0200

    dependencies: bump the spring-boot to 3.5.0

--- modules/cache/src/main/java/org/eclipse/xpanse/modules/cache/credential/CredentialCaffeineCacheExpiry.java
+++ modules/cache/src/main/java/org/eclipse/xpanse/modules/cache/credential/CredentialCaffeineCacheExpiry.java
@@ -9,7 +9,6 @@ import static org.eclipse.xpanse.modules.cache.consts.CacheConstants.DEFAULT_CAC
 
 import com.github.benmanes.caffeine.cache.Expiry;
 import java.util.concurrent.TimeUnit;
-import org.checkerframework.checker.index.qual.NonNegative;
 import org.eclipse.xpanse.modules.models.credential.AbstractCredentialInfo;
 
 /** Class to configure caffeine cache eviction policy for Credential Cache entries. */
@@ -24,13 +23,12 @@ public class CredentialCaffeineCacheExpiry implements Expiry<Object, Object> {
 
     @Override
     public long expireAfterUpdate(
-            Object key, Object value, long currentTime, @NonNegative long currentDuration) {
+            Object key, Object value, long currentTime, long currentDuration) {
         return currentDuration;
     }
 
     @Override
-    public long expireAfterRead(
-            Object key, Object value, long currentTime, @NonNegative long currentDuration) {
+    public long expireAfterRead(Object key, Object value, long currentTime, long currentDuration) {
         return currentDuration;
     }
 }
